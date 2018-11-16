2cbb9923217e7a763fdb55c1ac6e242003e9de32
MathDodo <mathiasdodo@hotmail.com>
2018-11-16 09:50:03 +0100

    - Stopping the saving thread

--- Assets/Scripts/_UnityObjects/ScriptableObjects/Singletons/SaveManager.cs
+++ Assets/Scripts/_UnityObjects/ScriptableObjects/Singletons/SaveManager.cs
@@ -35,7 +35,7 @@ public class SaveManager : RSingletonSO<SaveManager>
     {
         while (!destroyed)
         {
-            while (!save) { }
+            while (!save && !destroyed) { }
 
             if (save)
             {
